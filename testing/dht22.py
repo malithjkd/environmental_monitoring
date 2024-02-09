4c93f2d32f1668c434c029a8cc2f75e9fd07b3d5
malithjkd <malithjayawardhana@gmail.com>
2024-02-09 22:31:36 +0800

    sensor is setuped and recoding file

---
+++ testing/dht22.py
@@ -0,0 +1,21 @@
+import Adafruit_DHT
+import time
+from datetime import datetime
+
+DHT_SENSOR = Adafruit_DHT.DHT22
+DHT_PIN = 4
+
+file_name = datetime.now().strftime("%Y%m%d"+".txt") 
+
+while True:
+	file = open(file_name,'a')
+	humidity, temparature = Adafruit_DHT.read_retry(DHT_SENSOR,DHT_PIN)
+	timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+	if humidity is not None and temparature is not None:
+		output_string = f'{timestamp},{temparature:.1f},{humidity:.1f}'
+		#print("Temp = {0:.1f}*C , Humidity = {1:.1f}% ".format(temparature,humidity))
+		print(output_string)
+		time.sleep(2)
+		file.write(output_string + '\n')
+	else:
+		print("Sensor faliure")
