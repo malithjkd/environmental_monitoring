ac0bddffb1db4fd77c74a6311c4a93478f770a99
malithjkd <malithjayawardhana@gmail.com>
2024-02-11 23:49:14 +0800

    remove unwanted files, and modified the code to rcrate new fileon each day

--- testing/dht22.py
+++ testing/dht22.py
@@ -5,17 +5,18 @@ from datetime import datetime
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
 
-file_name = datetime.now().strftime("%Y%m%d"+".txt") 
-
 while True:
+	file_name = datetime.now().strftime("%Y%m%d"+".txt")
 	file = open(file_name,'a')
 	humidity, temparature = Adafruit_DHT.read_retry(DHT_SENSOR,DHT_PIN)
-	timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+	timestamp = datetime.now().strftime("%Y,%m,%d,%H,%M")
 	if humidity is not None and temparature is not None:
 		output_string = f'{timestamp},{temparature:.1f},{humidity:.1f}'
 		#print("Temp = {0:.1f}*C , Humidity = {1:.1f}% ".format(temparature,humidity))
 		print(output_string)
-		time.sleep(2)
 		file.write(output_string + '\n')
 	else:
 		print("Sensor faliure")
+
+	file.close()
+	time.sleep(60)
