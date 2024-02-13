5a07c580a5e562e2d22a5d6873018ae2b2338e2b
malithjkd <malithjayawardhana@gmail.com>
2024-02-14 01:40:01 +0800

    remove date now only time and values only terminial does not how anything

--- testing/dht22.py
+++ testing/dht22.py
@@ -4,16 +4,16 @@ from datetime import datetime
 
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
-
+print("Indoor Temparature and humidity measument started")
 while True:
 	file_name = datetime.now().strftime("%Y%m%d"+".txt")
 	file = open(file_name,'a')
 	humidity, temparature = Adafruit_DHT.read_retry(DHT_SENSOR,DHT_PIN)
-	timestamp = datetime.now().strftime("%Y,%m,%d,%H,%M")
+	timestamp = datetime.now().strftime("%H,%M")
 	if humidity is not None and temparature is not None:
 		output_string = f'{timestamp},{temparature:.1f},{humidity:.1f}'
 		#print("Temp = {0:.1f}*C , Humidity = {1:.1f}% ".format(temparature,humidity))
-		print(output_string)
+		#print(output_string)
 		file.write(output_string + '\n')
 	else:
 		print("Sensor faliure")
