aa189762186dfb843dac7c1b8253cc54804cdb44
K.D.M. Jayawardhana <malithjayawardhana@gmail.com>
2024-03-16 16:49:17 +0800

    add more led, connect to wifi and dht22 sensor

--- pico/blinking.py
+++ pico/blinking.py
@@ -1,12 +1,15 @@
 from machine import Pin
 import utime
 import network
-import machine 
+import machine
+import dht
 
 #led_pin = Pin('WL_GPIO0',machine.Pin.OUT) # Inbuild LED pin number name
 led_buildin = Pin("LED", Pin.OUT)	# they recoment to use inbuild LED 
 relay_1 = Pin(15,Pin.OUT)
 relay_2 = Pin(16,Pin.OUT)
+sensor = dht.DHT22(Pin(22))
+
 
 ssid = 'osiris-sg'
 password = 'bbppu35643'
@@ -26,3 +29,17 @@ def connect():
             print(f'Connected on {ip}')
             break
         x = x+1
+
+sensor.measure() 
+
+while True:
+    led_buildin.on()
+    relay_1.on()
+    relay_2.on()
+    utime.sleep_ms(2000)
+    led_buildin.off()
+    relay_1.off()
+    relay_2.off()
+    utime.sleep_ms(2000)
+    sensor.measure() 
+    print(sensor.temperature() ,sensor.humidity())
