e9ed63fa75a7351537b09221b2c931fb1f72e583
malithjkd <malithjayawardhana@gmail.com>
2024-04-03 21:13:22 +0800

    remove unwanted parts

--- pico/sensorair_test.py
+++ pico/sensorair_test.py
@@ -1,9 +1,13 @@
+# Raspberry pi pico w (micropython) UART communication with Sensorair S8
+# 2024.04.02
+# Malithjkd
+
+# import packages
 from machine import Pin
 import utime
 from machine import UART
 
-
-
+# Pins used
 uart = UART(1,baudrate=9600,tx=4,rx=5)
 uart.init(9600,bits=8, parity = None, stop=1)
 
@@ -11,10 +15,7 @@ led_buildin = Pin("LED", Pin.OUT)	# they recoment to use inbuild LED
 utime.sleep_ms(500)
 
 while True:
-    #led_buildin.on()
-    #utime.sleep_ms(200)
-    #led_buildin.off()
-    #utime.sleep_ms(200)
+
     uart.write(b"\xFE\x44\x00\x08\x02\x9F\x25")
     utime.sleep_ms(3000)
     data = uart.read(7)
@@ -22,17 +23,6 @@ while True:
     byte_3 = data[3]
     byte_4 = data[4]
     
-    #print(byte_3,byte_4)
-    #high = ord(data[3])
-    #low = ord(data[4])
     value = (byte_3*256)+byte_4    
-    #decoded_data = data.decode("utf-8")
     print(value)
-    
-    
-    # readline part also waorks
-    #uart.write(b"\xFE\x44\x00\x08\x02\x9F\x25")
-    #data_line = uart.readline()
-    #utime.sleep_ms(1000)
-    #print(data_line)
     
