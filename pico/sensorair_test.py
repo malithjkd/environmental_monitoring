9c19980ac50cafad19cbf67a324279437b278241
malithjkd <malithjayawardhana@gmail.com>
2024-04-10 16:36:42 +0800

    test finished

--- pico/sensorair_test.py
+++ pico/sensorair_test.py
@@ -19,7 +19,7 @@ while True:
     uart.write(b"\xFE\x44\x00\x08\x02\x9F\x25")
     utime.sleep_ms(3000)
     data = uart.read(7)
-    
+    #print(data)
     byte_3 = data[3]
     byte_4 = data[4]
     
