a01a55c5650cf5a90ea9aff108d2d4a829f28cb0
malithjkd <malithjayawardhana@gmail.com>
2024-06-11 00:05:25 +0800

    change high to low need more for loop

--- testing/relay_module.py
+++ testing/relay_module.py
@@ -5,21 +5,21 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 # Set up GPIO pins
-output_pins = [8, 10, 12, 16]  # List of GPIO pin numbers to be used as outputs
+output_pins = [14, 15, 18, 23]  # List of GPIO pin numbers to be used as outputs
 
 # Set all pins as output and initialize to LOW
 for pin in output_pins:
     GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, GPIO.LOW)
+    GPIO.output(pin, GPIO.HIGH)
 
 
 for pin in output_pins:
-    GPIO.output(pin, GPIO.HIGH)
-    print("Pin number: ",pin," is HIGH")
-    time.sleep(3)
     GPIO.output(pin, GPIO.LOW)
     print("Pin number: ",pin," is LOW")
-    time.sleep(3)
+    time.sleep(1)
+    GPIO.output(pin, GPIO.HIGH)
+    print("Pin number: ",pin," is HIGH")
+    time.sleep(1)
 
 
 # Clean up GPIO settings before exiting
