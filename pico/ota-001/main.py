1d2f4afd4f35d68dd6c35a79c2dbc98c7f410b49
malithjkd <malithjayawardhana@gmail.com>
2024-10-31 00:41:37 +0800

    update time and vertion

--- pico/ota-001/main.py
+++ pico/ota-001/main.py
@@ -7,6 +7,6 @@ led_onboard = machine.Pin("LED",machine.Pin.OUT)
 
 while True:
     led_onboard.on()
-    utime.sleep_ms(500)
+    utime.sleep_ms(50)
     led_onboard.off()
-    utime.sleep_ms(500)
+    utime.sleep_ms(50)
