266077479e5253abe0da6977d26b95f89b45f527
malithjkd <malithjayawardhana@gmail.com>
2025-03-31 15:33:13 +0800

    fix the bug with innitialization

--- pico/ota-001/controller.py
+++ pico/ota-001/controller.py
@@ -3,21 +3,25 @@ import utime
 import network
 import machine
 
+led_buildin = Pin("LED", Pin.OUT)
+relay_1 = Pin(15,Pin.OUT)
+relay_2 = Pin(16,Pin.OUT)
+        
+
 class controller():
     def __init__(self):
-        #led_pin = Pin('WL_GPIO0',machine.Pin.OUT) # Inbuild LED pin number name
-        led_buildin = Pin("LED", Pin.OUT)	# they recoment to use inbuild LED 
-        relay_1 = Pin(15,Pin.OUT)
-        relay_2 = Pin(16,Pin.OUT)
+        print("Controller initialized")
+        pass
+ 
         
 
     #def blink(timer1):
     #    led.toggle()
 
     def blink(self):
-        led.toggle()
+        led_buildin.toggle()
         utime.sleep_ms(1000)
-        led.toggle()
+        led_buildin.toggle()
         utime.sleep_ms(1000)
 
 
@@ -25,9 +29,9 @@ class controller():
         while True:
             led_buildin.on()
             #relay_1.on()
-            utime.sleep_ms(500)
+            utime.sleep_ms(200)
     
             led_buildin.off()
             #relay_1.off()
-            utime.sleep_ms(500)
+            utime.sleep_ms(200)
         
