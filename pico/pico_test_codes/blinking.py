1e28c09e2aba9e6f7fe2f3c5f514031f815cc50c
malithjkd <malithjayawardhana@gmail.com>
2026-07-15 08:47:24 +0800

    update pins

--- pico/pico_test_codes/blinking.py
+++ pico/pico_test_codes/blinking.py
@@ -6,9 +6,9 @@ import machine
 
 #led_pin = Pin('WL_GPIO0',machine.Pin.OUT) # Inbuild LED pin number name
 led_buildin = Pin("LED", Pin.OUT)	# they recoment to use inbuild LED 
-relay_1 = Pin(15,Pin.OUT)
-relay_2 = Pin(16,Pin.OUT)
-
+relay_1 = Pin(13,Pin.OUT)
+relay_2 = Pin(14,Pin.OUT)
+relay_3 = Pin(15,Pin.OUT)
 
 
 while True:
